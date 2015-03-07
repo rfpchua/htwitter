e8155555372668dace6e79f3a6974c76b34eaec5
Richard <rfpchua@gmail.com>
2015-03-07 17:57:45 -0500

    twitter style sign in

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,23 @@
 class TweetsController < ApplicationController
+
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
 	end
 
 	def create
 		@tweet = Tweet.new(tweet_params)
-		@tweet.save
-		render 'new'
+		@tweet.user = current_user 
+		if @tweet.save
+			flash[:success] = "You have created a tweet"
+			redirect_to new_tweet_path
+		end
+		
+	end
+
+	def index
+		@tweets = Tweet.all
 	end
 
 	def tweet_params
